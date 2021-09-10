ef4411b24cfadc31b7401a60c48b41e016f22d16
Zergs2001 <81860546+Zergs2001@users.noreply.github.com>
2021-09-10 20:58:38 +0700

    Update

--- MatRoleClaim/Controllers/ManageController.cs
+++ MatRoleClaim/Controllers/ManageController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MatRoleClaim.Models;
 using MatRoleClaim.Models.ViewModels;
+using MatRoleClaim.Models.IdentityModels;
+using System.Net;
 
 namespace MatRoleClaim.Controllers
 {
@@ -15,8 +17,16 @@ namespace MatRoleClaim.Controllers
     public class ManageController : BaseController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        public ActionResult Index(ManageMessageId? message)
+        public ActionResult Index(ManageMessageId? message, int? idcouse)
         {
+            if (idcouse != null && idcouse != 0)
+            {
+                ViewBag.DataCouser = db.Courses.Find(idcouse);
+            }
+            else
+            {
+                ViewBag.DataCouser = null;
+            }
             ViewBag.StatusMessage =
                 message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
                 : message == ManageMessageId.Error ? "An error has occurred."
@@ -95,5 +105,49 @@ namespace MatRoleClaim.Controllers
         }
 
         #endregion
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = DbContext.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            ApplicationUserViewModel applicationUserViewModel = new ApplicationUserViewModel { Id = applicationUser.Id, Email = applicationUser.Email, NewPassword = "", UserName = applicationUser.UserName, DateOfBirth = applicationUser.DateOfBirth, Phone = applicationUser.Phone, Name = applicationUser.Name, Address = applicationUser.Address };
+            return View(applicationUserViewModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Email,NewPassword,UserName,DateOfBirth,Phone,Address,Name")] ApplicationUserViewModel applicationUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                ApplicationUser applicationUser = DbContext.Users.Find(applicationUserViewModel.Id);
+                applicationUser.UserName = applicationUserViewModel.UserName;
+                applicationUser.Phone = applicationUserViewModel.Phone;
+                applicationUser.DateOfBirth = applicationUserViewModel.DateOfBirth;
+                applicationUser.Name = applicationUserViewModel.Name;
+                applicationUser.Address = applicationUserViewModel.Address;
+                if (applicationUser == null)
+                    return HttpNotFound();
+
+                IdentityResult result = null;
+                if (!String.IsNullOrEmpty(applicationUserViewModel.NewPassword))
+                {
+                    var token = await UserManager.GeneratePasswordResetTokenAsync(applicationUser.Id);
+                    result = await UserManager.ResetPasswordAsync(applicationUser.Id, token, applicationUserViewModel.NewPassword);
+                }
+                var result1 = await UserManager.UpdateAsync(applicationUser);
+                return Redirect("/Manage");
+            }
+
+            return View(applicationUserViewModel);
+        }
     }
 }
